8560dfaf235084500f9180e04ec6fed24944e209
Matthew Wessel <nucmatt@gmail.com>
2020-10-14 13:14:17 -0500

    Build: Basic format for drum-pad
    Got a single drum pad button on screen and audio to play with button
    press and keydown event.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './scss/style.scss';
 
 function App() {
-  return (
-    <div>Hello World!
-    </div>
-  );
+	const playSound = () => {
+    const audio = document.getElementById('Q');
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  const keyHandler = (e) => {
+    if (e.keyCode === 81) {
+      playSound();
+    }
+  }
+  
+  useEffect(() => {
+    window.addEventListener("keydown", keyHandler);
+    return () => {
+      window.removeEventListener("keydown", keyHandler);
+    }
+  })
+	return (
+		<div id='drum-machine'>
+			<div className='display'>
+				<button
+					className='drum-pad btn'
+					id='highhat1'
+					onClick={() => playSound()}
+				>
+					<audio
+						src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+						className='clip'
+						id='Q'
+					></audio>
+					Q
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
